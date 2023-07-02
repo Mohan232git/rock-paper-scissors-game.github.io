1d576ff8d113b08c0ecbefd0ecdaa1f0430b60fd
Gituser <pvmohan232@gmail.com>
2023-07-02 22:14:11 +0530

    update

---
+++ index.js
@@ -0,0 +1,7 @@
+function handpicked(hand){
+    const container = document.querySelector(".container");
+    console.log(container);
+    const handpicked = document.querySelector(hand);
+    console.log(handpicked)
+    container.style.display="none";
+}
