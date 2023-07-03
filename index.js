a22c5d466800912de300114238605fc1e62b4fdf
Gituser <pvmohan232@gmail.com>
2023-07-03 23:05:45 +0530

    update

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 function handpicked(hand){
     const container = document.querySelector(".container");
+    const competediv = document.querySelector(".compete");
+    let newImage = document.getElementById("newImage");
     console.log(container);
     const handpicked = document.querySelector(hand);
+     let selectedimg =handpicked.querySelector('img')
     console.log(handpicked)
     container.style.display="none";
+    competediv.style.display="grid";
+    console.log(newImage)
+    newImage.src = selectedimg.src;
+    let borderstyle = handpicked.style.outlineColor;
+    console.log(borderstyle)
+}
+
+function playagain(){
+    const container = document.querySelector(".container");
+    const competediv = document.querySelector(".compete");
+        competediv.style.display="none";
+        container.style.display="flex";
+
 }
