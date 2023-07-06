9e1a4f05ba9005f856560d678700fe02c3e8c2f8
Gituser <pvmohan232@gmail.com>
2023-07-06 23:42:56 +0530

    update

--- index.js
+++ index.js
@@ -4,29 +4,44 @@ function handpicked(hand){
     let generatedhand = document.querySelector('.generated-hand');
     let generatedimg = generatedhand.querySelector('img');
     let newImage = document.getElementById("newImage");
+    let pickedDiv = document.querySelector('.picked-div');
     const handpicked = document.querySelector(hand);
     let selectedimg =handpicked.querySelector('img')
+
     container.style.display="none";
-    competediv.style.display="grid";
-    newImage.src = selectedimg.src;
-    
+    competediv.style.display="grid"; 
+    pickedDiv.innerHTML = handpicked.innerHTML;
+    pickedDiv.classList.add(hand)
+    console.log(pickedDiv)
 }
 
 function playagain(){
     const container = document.querySelector(".container");
     const competediv = document.querySelector(".compete");
+    
         competediv.style.display="none";
         container.style.display="flex";
+      
 
 }
 
 
 function closerules() {
      const rulescontainer = document.querySelector('.Rules-container');
-      rulescontainer.style.display ='none'
+     const rulesbtn = document.querySelector('.btn')
+     
+      rulescontainer.style.display ='none';
+      rulesbtn.style.display='block'
+      
+
 }
 
 function openrules(){
     const rulescontainer = document.querySelector('.Rules-container');
-    rulescontainer.style.display='block'
+    
+    const rulesbtn = document.querySelector('.btn')
+    rulesbtn.style.display='none'
+    rulescontainer.style.display='inline-block';
+    rulescontainer.style.background='red'
+  
 }
